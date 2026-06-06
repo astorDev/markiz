1c51337cf27e79052738bb667b60fdaf2ee06c9f
Egor Tarasov <vosarat1995@google.com>
2026-06-06 06:12:29 +0200

    initial code changes

---
+++ get/lib/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Markiz.Get;
+
+public class MarkdownSegmentExtractorManager(IEnumerable<Func<string, IMarkdownSegmentDirective?>> directiveFactories)
+{
+    public string Extract(IEnumerable<string> markdownLines, string directive)
+    {
+        foreach (var directiveFactory in directiveFactories)
+        {
+            var parsedDirective = directiveFactory(directive);
+            if (parsedDirective != null)
+            {
+                // Process the directive
+            }
+        }
+
+        throw new NotImplementedException();
+    }
+}
+
+public interface IMarkdownSegmentDirective
+{
+    static abstract IMarkdownSegmentDirective? From(string directivePart);
+    bool MatchesBeginningOfSegment(string line);
+    bool MatchesEndOfSegment(string line);
+}
